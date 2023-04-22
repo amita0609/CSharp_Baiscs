b73bac765dbf7c8ac4bef0280c3d264b3b04054d
Amita Tiwari <at224217@gmail.com>
2023-04-22 22:59:09 +0530

    first commit

---
+++ C#_Properties/C#_Properties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            e1.Name = "Sonoo Jaiswal";
+            Console.WriteLine("Employee Name: " + e1.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#_Struct/C#_Struct/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static C__Struct.Rectangle;
+
+namespace C__Struct
+{
+    public struct Rectangle
+    {
+        public int width, height;
+
+        public enum Days { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+
+        public Rectangle(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+        public void areaOfRectangle()
+        {
+            Console.WriteLine("Area of Rectangle is: " + (width * height));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(5, 6);
+            r.areaOfRectangle();
+
+
+
+            //There are amny ways to access enum
+
+            int x = (int)Days.Sun;
+            int y = (int)Days.Mon;
+            int z = (int)Days.Sat;
+            Console.WriteLine("Sun = {0}", x);
+            Console.WriteLine("Mon = {0}", y);
+            Console.WriteLine("Sat = {0}", z);
+
+
+            foreach (Days d in Enum.GetValues(typeof(Days)))  //getValues() also method to get values.
+            {
+                Console.WriteLine(d);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
